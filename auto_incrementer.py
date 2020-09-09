77dee79e0f00a91e7bc1459a779c29d8333210e8
Zachary Faddis <zach@zfadd.is>
2020-09-08 20:57:00 -0500

    remove new formatting string

--- auto_incrementer.py
+++ auto_incrementer.py
@@ -6,10 +6,10 @@ from goal import get_all_goals
 def auto_increment(dry_run=True):
     for goal in get_all_goals():
         if goal.safe_to_increment():
-            print(f"incrementing goal {goal.name} from {goal.rate} to {goal.rate+goal.step_rate} a {goal.runits}")
+            print("incrementing goal %s from %s to %s a %s" % (goal.name, goal.rate, goal.rate+goal.step_rate, goal.runits))
             goal.increment_rate(dry_run=dry_run)
         else:
-            print(f"goal {goal.slug} not safe to increment")
+            print("goal %s not safe to increment" % goal.slug)
 
 
 if __name__ == "__main__":
